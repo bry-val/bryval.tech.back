55f42d85feb22d4c93d24fc23cbba4d6c377e180
Bryan Valencia <94031627+bry-val@users.noreply.github.com>
2024-04-22 15:18:02 -0500

    Update getCount.py

--- lambdas/getCount.py
+++ lambdas/getCount.py
@@ -4,11 +4,7 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('bryval-update')
 
-    response = table.get_item(
-        Key={
-            'counter': 0
-        }
-    )
+    response = table.get_item(Key={ 'counter': 0 })
 
     item = response['Item']
     count_value = item['count']
